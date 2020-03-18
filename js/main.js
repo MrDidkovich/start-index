c85dd68c07b3d98c80f908b8a4c99e6ada489132
Konstantin <ksodar74ru@gmail.com>
2020-03-18 19:57:29 +0500

    feat: add yandex map and text in footer

--- js/main.js
+++ js/main.js
@@ -75,4 +75,38 @@ $(document).ready(function () {
 	});
 	//маска для телефона
 	$('[type=tel').mask('+7(000) 00-00-00', {placeholder: "+7(___) __-__-___"});
+
+	//создание Яндекс.Карты
+	ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [55.751574, 37.573856],
+            zoom: 9
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Это красивая метка'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/marker.png',
+            // Размеры метки.
+            iconImageSize: [32, 32],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        });
+
+    myMap.geoObjects
+        .add(myPlacemark);
+	});
 });
